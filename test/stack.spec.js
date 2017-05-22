3e5c9af9b10895666289c3f6d9a74fd1d8c6c431
Yauhen <penkin.evgeniy@gmail.com>
2017-05-22 12:44:22 +0300

    test (stack): other tests added

--- test/stack.spec.js
+++ test/stack.spec.js
@@ -63,4 +63,24 @@ describe('Stack', () => {
       expect(st.top).to.equal(null);
     });
   });
+
+  describe('#getSize', () => {
+    const st = new Stack();
+
+    st.append('node1');
+    st.append('node2');
+    st.append('node3');
+
+    it('assigns 3 to this.size', () => {
+      expect(st.size).to.equal(3);
+    });
+
+    it('assigns 0 to this.size', () => {
+      st.pop();
+      st.pop();
+      st.pop();
+      expect(st.size).to.equal(0);
+    });
+  });
+
 });
